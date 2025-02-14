e68324311fe100232b0afa579ffeb6b1f8b9db43
Sunny Gupta <sunnygupta4924@gmail.com>
2025-02-15 00:38:21 +0530

    created index route to show all listings using get request for path /listings

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app=express();
 let port =8080;
 const mongoose=require("mongoose");
 const Listing=require("./models/listing.js")
+const path=require("path");
+
 
 
 async function main(){
@@ -22,21 +24,30 @@ app.listen(port,()=>{
 
 });
 
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+
 app.get("/",(req,res)=>{
     res.send("hi working");
 })
 
-app.get("/testinglist", async (req,res)=>{
+// app.get("/testinglist", async (req,res)=>{
     
-    //creating new object or document
-    let samplelisting=new Listing({
-        title:"my room",
-        description:"in usa",
-        price:1222,
-        location:"washington dc",
-        country:"usa"
-    })
-  await  samplelisting.save();
-  console.log("sample was saved");
-  res.send("sucess")
+//     //creating new object or document
+//     let samplelisting=new Listing({
+//         title:"my room",
+//         description:"in usa",
+//         price:1222,
+//         location:"washington dc",
+//         country:"usa"
+//     })
+//   await  samplelisting.save();
+//   console.log("sample was saved");
+//   res.send("sucess")
+// })
+
+app.get("/listings",async (req,res)=>{
+  const alllistings= await Listing.find({});
+  res.render("listings/index.ejs",{alllistings});
 })
+
